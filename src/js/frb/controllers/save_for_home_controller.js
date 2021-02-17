b7367497fdbafe6c526aebcf64d355497067541a
Mark Healey <markhealey@users.noreply.github.com>
2021-02-17 09:32:18 -0700

    fix home

--- src/js/frb/controllers/save_for_home_controller.js
+++ src/js/frb/controllers/save_for_home_controller.js
@@ -42,7 +42,7 @@ export default class extends Controller {
     const defaults = {
       Homeowner_Association_Fees: 0,
       Homeowner_Insurance_Costs_Monthly: 1000/12,
-      // Home_Property_Taxes: 5000,
+      Home_Property_Taxes: 5000,
       Mortgage_Down_Payment_Pct: .2,
       Mortgage_Insurance_Per_Month: 0,
       Mortgage_Interest_Rate: .03,
